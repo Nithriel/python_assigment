d77a4fa1bc3454416a0719ab4ea427400a1bf298
Raphael Pletz <raphael.pletz00@gmail.com>
2019-04-08 19:19:13 -0700

    newest

--- main_app_controller.py
+++ main_app_controller.py
@@ -2,8 +2,11 @@ import tkinter as tk
 from top_navbar_view import TopNavbarView
 from page1_view import Page1View
 from page2_view import Page2View
+from staff_view import StaffView
+from player_add import PlayerView
+from details_staff import StaffDetails
 from bottom_navbar_view import BottomNavbarView
-from popup_view import PopupView
+from tkinter import messagebox as tkMessageBox
 import requests
 
 
@@ -14,9 +17,9 @@ class MainAppController(tk.Frame):
         """ Initialize Main Application """
         tk.Frame.__init__(self, parent)
 
-        self._top_navbar = TopNavbarView(self, self._page_callback, self._page_popup_callback)
-        self._page1 = Page1View(self, self._page1_submit_callback)
-        self._page2 = Page2View(self, self._page2_submit_callback)
+        self._top_navbar = TopNavbarView(self, self._page_callback)
+        self._page1 = Page1View(self, self._page1_refresh_callback, self._page_1_delete_callback, self._page1_popup_callback, self._page1_details_callback)
+        self._page2 = Page2View(self, self._page2_refresh_callback, self._page_2_delete_callback, self._page2_popup_callback, self._page1_details_callback)
         self._bottom_navbar = BottomNavbarView(self, self._quit_callback)
 
         self._top_navbar.grid(row=0, columnspan=4, pady=10)
@@ -32,39 +35,74 @@ class MainAppController(tk.Frame):
 
     def _page_callback(self):
         """ Handle Switching Between Pages """
-
         curr_page = self._top_navbar.curr_page.get()
-        if (self._curr_page != curr_page and self._curr_page == TopNavbarView.PAGE1):
+        if self._curr_page != curr_page and self._curr_page == TopNavbarView.PAGE1:
             self._page1.grid_forget()
             self._page2.grid(row=1, columnspan=4)
             self._curr_page = TopNavbarView.PAGE2
-        elif (self._curr_page != curr_page and self._curr_page == TopNavbarView.PAGE2):
+        elif self._curr_page != curr_page and self._curr_page == TopNavbarView.PAGE2:
             self._page2.grid_forget()
             self._page1.grid(row=1, columnspan=4)
             self._curr_page = TopNavbarView.PAGE1
 
-    def _page_popup_callback(self):
+    def _page1_refresh_callback(self):
+        print("Refresh Page Staff")
+        self._page1.set_form_data(self._call_requests('staff'))
+
+    def _page2_refresh_callback(self):
+        print("Refresh Page Player")
+        self._page2.set_form_data(self._call_requests('player'))
+
+    def _page_1_delete_callback(self):
+        if tkMessageBox.askyesno('Verify', 'Really delete?'):
+            # Delete item
+            index = self._page1._list_box.curselection()[0]
+            print("Index: " + str(index))
+            staff_list = self._call_requests('staff')
+            self._delete_id(staff_list[index]['id'])
+            self._page1_refresh_callback()
+
+    def _page_2_delete_callback(self):
+        if tkMessageBox.askyesno('Verify', 'Really delete?'):
+            # Delete item
+            index = self._page2._list_box.curselection()[0]
+            print("Index: " + str(index))
+            player_list = self._call_requests('player')
+            self._delete_id(player_list[index]['id'])
+            self._page2_refresh_callback()
+
+    def _page2_popup_callback(self):
         self._popup_win = tk.Toplevel()
-        self._popup = PopupView(self._popup_win, self._close_popup_callback)
+        self._popup = PlayerView(self._popup_win, self._close_popup_callback)
+
+    def _page1_popup_callback(self):
+        self._popup_win = tk.Toplevel()
+        self._popup = StaffView(self._popup_win, self._close_popup_callback)
 
     def _close_popup_callback(self):
         self._popup_win.destroy()
 
-    def _page1_submit_callback(self):
-        print("Refresh Page Staff")
-        print(self._page1.set_form_data(self._call_requests('staff')))
-
-    def _page2_submit_callback(self):
-        print("Refresh Page Player")
-        print(self._page1.set_form_data(self._call_requests('player')))
-
     def _quit_callback(self):
         self.quit()
+
+    def _page1_details_callback(self):
+        index = self._page1._list_box.curselection()[0]
+        print("Index: " + str(index))
+        staff_list = self._call_requests('staff')
+        response = requests.get("http://127.0.0.1:5000/team_manager/employee/" + str(staff_list[index]['id']))
+        response_json = response.json()
+        print(response_json)
+        self._popup_win = tk.Toplevel()
+        self._popup = StaffDetails(self._popup_win, self._close_popup_callback)
 
     def _call_requests(self, entity_type):
         response = requests.get("http://127.0.0.1:5000/team_manager/employee/all/" + entity_type)
         if response.status_code == 200:
             return response.json()
+
+    @staticmethod
+    def _delete_id(id):
+        requests.delete("http://127.0.0.1:5000/team_manager/employee/" + str(id))
 
 if __name__ == "__main__":
     root = tk.Tk()
--- page2_view.py
+++ page2_view.py
@@ -1,41 +1,64 @@
 import tkinter as tk
-
+from tkinter import messagebox as tkMessageBox
 
 class Page2View(tk.Frame):
     """ Page 2 """
 
-    def __init__(self, parent, submit_callback):
+    def __init__(self, parent, submit_callback, delete_callback, popup_callback, details_callback):
         """ Initialize Page 2 """
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent, width=800, height=800)
         self._parent = parent
 
         self._submit_callback = submit_callback
+        self._delete = delete_callback
+        self._popup_callback = popup_callback
+        self._details_callback = details_callback
 
         self._create_widgets()
 
     def _create_widgets(self):
         """ Creates the widgets for Page 2 """
         self._scrollbar = tk.Scrollbar(self, orient=tk.VERTICAL)
-        self._label = tk.Label(self, text="Player:")
+        self._label = tk.Label(self)
         self._label.grid(row=1, column=0, padx=20)
 
         self._list_box = tk.Listbox(self)
-        self._list_box.grid(row=2)
+        self._list_box.grid(row=2, ipadx=30, columnspan=2)
 
         self._scrollbar.config(command=self._list_box.yview)
-        self._scrollbar.grid(row=2, column=3, sticky=tk.N + tk.S + tk.W + tk.E)
+        self._scrollbar.grid(row=2, column=4, sticky=tk.N + tk.S + tk.W + tk.E)
 
-        self._button = tk.Button(self,
-                                 text="Refresh",
+        self._add_button = tk.Button(self,
+                                 text="Add",
+                                 command=self._popup_callback)
+        self._add_button.grid(row=4, column=0, columnspan=1, sticky=tk.W + tk.E)
+
+        self._update_button = tk.Button(self,
+                                 text="Update",
                                  command=self._submit_callback)
-        self._button.grid(row=3, padx=20)
+        self._update_button.grid(row=4, column=1, columnspan=1, sticky=tk.W + tk.E)
+
+        self._delete_button = tk.Button(self,
+                                 text="Delete",
+                                 command=self._delete)
+        self._delete_button.grid(row=5, column=0, columnspan=1, sticky=tk.W + tk.E)
+
+        self._details_button = tk.Button(self,
+                                        text="Details",
+                                        command=self._details_callback)
+        self._details_button.grid(row=5, column=1, columnspan=1, sticky=tk.W + tk.E)
+
+        self._refresh_button = tk.Button(self,
+                                         text="Refresh",
+                                         command=self._submit_callback)
+        self._refresh_button.grid(row=7, columnspan=2, sticky=tk.E + tk.W)
 
     # def get_form_data(self):
     #     return { "birthdate": self._entry_birthdate.get() }
     def set_form_data(self, data):
         self._list_box.delete(0, tk.END)
         for item in data:
-            self._list_box.insert(tk.END, item['first_name'])
+            self._list_box.insert(tk.END, item['first_name'] + ' ' + item['last_name'] + ', ' + item['position'])
 
 
 
